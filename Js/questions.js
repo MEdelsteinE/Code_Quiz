6c7ad322419ce5318dce3ab0d8a4b59d80ce6f0f
MedelsteinE <medelstein1123@gmail.com>
2023-09-13 23:39:15 -0400

    Added more questions and choices

--- Js/questions.js
+++ Js/questions.js
@@ -1,39 +1,63 @@
 var questions = [
     {
         question: 'What does HTML stand for?',
-        choices: [''],
-        answer: 'HyperTextMarupLanguage'
+        choices: ['HperText Marker Language ', 'HyperText Madeup Language ', 'HyperText Markup Language', 'HyperText Makeup Language'],
+        answer: 'HyperText Markup Language'
     },
 
     {
         question:'What does CSS stand for?',
-        choices: [''],
+        choices: ['Code Style Sheet', 'Cascading Style Sheet', 'Create Style Sheet', 'Cascading Syntax Sheet'],
         answer: 'Cascading Style Sheet'
 
     },
 
     {
         question:'What does JS stand for?',
-        choices: [''],
+        choices: ['JavaScience', 'JavaSheet', 'JavaStyle', 'JavaScript'],
         answer: 'JavaScript'
     },
 
     {
         question: 'Where does the Title element appear on the  Web Application?',
-        choices: [''],
+        choices: ['The Page tab', 'In the header', 'At the bottom of the page', 'In the address bar'],
         answer: 'The Page tab'
     },
 
     {
         question: 'How do you select a class in CSS?',
-        choices: [''],
+        choices: ['With the hashtag selector', 'With a dollar sign selector', 'With the dot selector', 'With an Exclamation Point'],
         answer: 'With the dot selector'
     },
 
     {
         question: 'How do you select an id in CSS?',
-        choices: [''],
-        answer: 'with the hashtag selector'
+        choices: ['With the hashtag selector', 'With the dot selector', 'With an ampersan', 'With a Percent Symbol'],
+        answer: 'With the hashtag selector'
     },
+
+    {
+        question: 'How do you set Flex in CSS?',
+        choices: ['CSS: Flex', 'Display: CSS_Flex', 'Display: flex', 'Flex: display'],
+        answer: 'Display: flex'
+    },
+
+    {
+        question: 'Where do you link a JavaScript file a HTML file?',
+        choices: ['Above the Head Element', 'Under the Closing HTML Tag', 'At the bottom above the Closing Body Tag', 'In a <p> element'  ],
+        answer: 'At the bottom above the Closing Body Tag'
+    },
+
+    {
+        question: 'Where do you link a CSS file in an HTML file?',
+        choices: ['Above the DOCTYPE Declaration', 'Above the title property', 'In a <div> Element', 'With an ID'],
+        answer: 'Above the title property'
+    },
+
+    {
+        question: 'How do you have a link open a new tab in the browser?',
+        choices: ['Not Possible', '_blank', 'With another HyperLink', 'In the JS File'],
+        answer: '_blank'
+    }
 
 ]
